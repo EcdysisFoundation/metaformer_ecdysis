812654506f7e419e71d59fdfabba361e04842bb2
Mikayla Wilson <mikaylaelectra@gmail.com>
2024-12-10 12:50:14 -0500

    cleanup2

--- dataset_generation/__main__.py
+++ dataset_generation/__main__.py
@@ -2,15 +2,12 @@ import argparse
 import logging
 from pathlib import Path
 
-import sys
-sys.path.append(r"")
 from .data import BugBoxData
 from .split import split_from_df, generate_split_class_report
 from .utils import drop_identical_images
 
 from . import LOGGING_LEVEL, INFO
 
-TAXON_LEVELS = levels = ['order', 'family', 'genus']
 SEED = 42
 LOGGING_LEVEL = INFO
 
@@ -25,9 +22,7 @@ def get_args() -> argparse.Namespace:
     parser.add_argument('--bugbox-mnt', type=str, default='/pool1/srv/bugbox3/bugbox3/media/',
                         help='Path to BugBox images mounted directory')
     parser.add_argument('--train-size', type=float, default=0.6, help='Relative size of the train split')
-    parser.add_argument('--dataset-name', type=str, default='bugbox', help='Name of the generated dataset')
-    parser.add_argument('--reference-image', type=str, default='/pool1/ref_images/gen2-19-vcm/',
-                        help='Path to reference images root directory')
+    parser.add_argument('--dataset-name', type=str, default='bugbox_model_1', help='Name of the generated dataset')
     parser.add_argument('--drop-duplicates', action='store_true', help='Drop duplicate images from the dataset')
     parser.add_argument('--hard-copy', action='store_true', help='Copy images instead of symlinking them')
     parser.add_argument('--minimum-images', type=int, default=20, help='Do not create a class unless it has at least '
@@ -42,30 +37,18 @@ def main():
     assert 0 < args.train_size <= 1, 'Train size must be between 0 and 1'
 
     bugbox_mnt = Path(args.bugbox_mnt)
-    refimage_dir = Path(f'{args.reference_image}')
     dataset_dir = Path(f'datasets/{args.dataset_name}')
-
     dataset_dir.mkdir(exist_ok=True, parents=True)
 
     db = BugBoxData()
-
- #   reviewed_images = db[['morphos_name','morphos_id','specimen_id','uuid','image', 'specimen_id']]
-#    print(reviewed_images.head()) 
     images = db.get_reviewed_images_df()
-    
-#    print(images[images['morphos_id'] == '3458'])
- #   print(images)
     images['image'] = images['image'].apply(lambda x: str(bugbox_mnt / x))
-   
+
     if args.drop_duplicates:
         images = drop_identical_images(images)
 
-    
-    print(images['morphos_name'].nunique())
-    refimages = None
-
     splits = split_from_df(images, args.train_size, dataset_dir, not args.hard_copy,
-                   seed=SEED, refimages=refimages, min_images=args.minimum_images)
+                   seed=SEED, min_images=args.minimum_images)
 
 
     meta_file = dataset_dir/'metadata.csv'
@@ -73,11 +56,9 @@ def main():
 
     taxon_map = db.get_morphospecies_df(columns=['morphos_id', 'morphos_name','specimen_id'])
     taxon_map.to_csv('deploy/taxon_map.csv', index=False)
-        
+
     report_count_df = generate_split_class_report(splits, taxon_map)
     report_count_df.to_csv(dataset_dir / 'dataset_report.csv', index=False)
-
-
 
 # This gets executed when running `python -m dataset_generation`
 if __name__ == '__main__':
--- dataset_generation/data.py
+++ dataset_generation/data.py
@@ -1,16 +1,6 @@
-import logging
 from typing import List
 
 import pandas as pd
-# local imports
-from . import LOGGING_LEVEL, INFO
-
-TAXON_LEVELS = levels = ['order', 'family', 'genus']
-SEED = 42
-LOGGING_LEVEL = INFO
-
-logger = logging.getLogger(__name__)
-logger.setLevel(LOGGING_LEVEL)
 
 
 class BugBoxData:
@@ -18,34 +8,21 @@ class BugBoxData:
     def get_df(self: str):
         """
         Make a dataframe from a csv
-
-
         Returns: Panda's DataFrame with the results of the query
         """
 
         specimen_df = pd.read_csv('dataset_generation/training_selections.csv')
-        print(specimen_df.columns.tolist())
-        print(specimen_df['morphos_name'].nunique())
-        print(specimen_df.shape)
-        #specimen_df = specimen_df.groupby(specimen_df['morphos_name']).filter(lambda x: len(x) >= 20).reset_index(drop=True)
-        print(specimen_df['morphos_name'].nunique())
-        print(specimen_df.shape)
         return pd.DataFrame(specimen_df)
 
     def get_reviewed_images_df(self, columns: List[str] = None) -> pd.DataFrame:
         """
         Get table of reviewed images
-
         Args:
             columns: Subset of columns
-            lookback: Interval to query for new images; could be day, week, month, year. If None, it does not perform
-            filtering by date
-
         Returns: Pandas DataFrame
         """
 
         reviewed_images = self.get_df()
-#        print(reviewed_images)
         reviewed_images = reviewed_images[['morphos_name', 'morphos_id', 'specimen_id', 'image', 'specimen_count']]
 
         if columns is not None:
@@ -57,7 +34,6 @@ class BugBoxData:
     def get_morphospecies_df(self, columns: List[str] = None):
         """
         Get table of morphospecies ids
-
         Args:
             columns: Subset of columns
 
@@ -65,17 +41,8 @@ class BugBoxData:
         """
 
         morphospecies = self.get_df()
-        
+
         if columns is not None:
             morphospecies = morphospecies[columns]
 
         return morphospecies
-
-
-if __name__ == '__main__':
-    df = BugBoxData()
-
-    images = df.get_reviewed_images_df(columns=[['image', 'morphos_id']], lookback='week')
-
-    print(images.head())
-
