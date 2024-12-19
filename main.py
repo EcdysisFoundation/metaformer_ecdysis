726419334c085c5d3c709866cfb6a428bea271b4
Mikayla Wilson <mikaylaelectra@gmail.com>
2024-12-19 09:45:17 -0500

    delete commented out

--- main.py
+++ main.py
@@ -429,10 +429,7 @@ def validate(config, data_loader, model, epoch, metric, mask_meta=False, tb_logg
     if config.EVAL_MODE and dist.get_rank() == 0:
 
         display = 3 # how many entries to display for debug purposes
-        # classes_df =  pd.read_csv('deploy/taxon_map.csv')
-        # logger.info(f"Columns and first entries from taxon map are:\n{classes_df.head(display)}")
         logger.info(f"First class entries are:\n{list(config.DATA.CLASS_NAMES)[:display]}")
-        # class_ids = classes_df.loc[map(int, config.DATA.CLASS_NAMES)].reset_index()[id_column]
         stats = get_stats(metric, list(config.DATA.CLASS_NAMES), Path(config.OUTPUT)/f'stats.csv', config.VERSION, save_csv=True)
         split_report_path = Path(config.OUTPUT)/f'dataset_report.csv'
         split_df = pd.read_csv(split_report_path) if split_report_path.exists() else None
