377ad62ecf8b74075687391acc18642fab56a921
Mikayla Wilson <mikaylaelectra@gmail.com>
2026-06-16 12:17:00 -0400

    output directory based on the dataset, with a subdirectory based on the version

--- deploy/copy_reports.sh
+++ deploy/copy_reports.sh
@@ -8,8 +8,8 @@ DATASET=$1
 OUTPUT_DIR=$2
 THIS_VERSION=$3
 # As training can take long it's better to avoid referencing the dataset folder, put all required files in the model folder
-mkdir -p "${OUTPUT_DIR}${DATASET}/${THIS_VERSION}"
+mkdir -p "${OUTPUT_DIR}/${DATASET}/${THIS_VERSION}"
 
-cp "datasets/${DATASET}/dataset_report.csv" "${OUTPUT_DIR}${DATASET}/${THIS_VERSION}/dataset_report.csv"
-cp "datasets/${DATASET}/morphospecies_map.csv" "${OUTPUT_DIR}${DATASET}/${THIS_VERSION}/morphospecies_map.csv"
-cp "dataset_generation/training_selections.csv" "${OUTPUT_DIR}${DATASET}/${THIS_VERSION}/training_selections.csv"
+cp "datasets/${DATASET}/dataset_report.csv" "${OUTPUT_DIR}/${DATASET}/${THIS_VERSION}/dataset_report.csv"
+cp "datasets/${DATASET}/morphospecies_map.csv" "${OUTPUT_DIR}/${DATASET}/${THIS_VERSION}/morphospecies_map.csv"
+cp "dataset_generation/training_selections.csv" "${OUTPUT_DIR}/${DATASET}/${THIS_VERSION}/training_selections.csv"
--- deploy/training.sh
+++ deploy/training.sh
@@ -5,16 +5,18 @@
 set -eE  # Exit if any command fails https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 
 # run script, writing output to log
-# bash deploy/training.sh DATASET_NAME STARTING_CHECKPOINT THIS_VERSION > training_run.log 2>&1 &
+# bash deploy/training.sh DATASET_NAME STARTING_CHECKPOINT_DIR THIS_VERSION > training_run.log 2>&1 &
+# example, note 6/6 is refering to the directory structure where dataset=6, previous version = 6
+#          This assumes a directory based on the dataset, with a subdirectory based on the version
 # bash deploy/training.sh 6 6/6 testing6 > training_run.log 2>&1 &
 # view training log for status updates
 # tail -f training_run.log
 
 cd /home/ecdysis/MetaFormer/
 
-OUTPUT_DIR="output/ecdysis/"
+OUTPUT_DIR="output/ecdysis"
 DATASET_NAME="$1"
-STARTING_CHECKPOINT="$2"
+STARTING_CHECKPOINT_DIR="$2"
 THIS_VERSION="$3"
 
 export PYTHONUNBUFFERED=1
@@ -28,9 +30,9 @@ echo "Found ${GPU_COUNT} GPU(s)"
 # Run training starting from last best checkpoint
 /home/ecdysis/miniconda3/envs/pytorch/bin/torchrun --nproc_per_node ${GPU_COUNT} main.py --cfg configs/ecdysis_test.yaml \
  --data-path "datasets/${DATASET_NAME}/" --tag "$1" --version "$THIS_VERSION" \
-  --pretrain "${OUTPUT_DIR}/${STARTING_CHECKPOINT}/best.pth"
+  --pretrain "${OUTPUT_DIR}/${STARTING_CHECKPOINT_DIR}/best.pth"
 wait
 # Evaluate trained model
-#/home/ecdysis/miniconda3/envs/pytorch/bin/torchrun --nproc_per_node ${GPU_COUNT} main.py \
-#  --cfg "${OUTPUT_DIR}/${THIS_VERSION}/config.yaml" --dataset bugbox --data-path "datasets/${DATASET_NAME}" --eval  --pretrain "${OUTPUT_DIR}/${THIS_VERSION}/best.pth" --version "$THIS_VERSION"
-#wait
+/home/ecdysis/miniconda3/envs/pytorch/bin/torchrun --nproc_per_node ${GPU_COUNT} main.py \
+  --cfg "${OUTPUT_DIR}/${DATASET_NAME}/${THIS_VERSION}/config.yaml" --dataset bugbox --data-path "datasets/${DATASET_NAME}" --eval  --pretrain "${OUTPUT_DIR}/${DATASET_NAME}/${THIS_VERSION}/best.pth" --version "$THIS_VERSION"
+wait
